8235f5f82a00585cc3a0851eb92e649c6b927ad5
sansangood <40737037+sansangood@users.noreply.github.com>
2023-11-04 06:25:30 +0000

    modify

---
+++ lesson7_2.py
@@ -0,0 +1,14 @@
+import tools
+
+def main():
+    while(True):
+        tools.playGame()
+        playAgain = input ("您還要繼續嗎(y,n)?")
+        if playAgain =='n':
+            break
+        
+    print("遊戲結束")
+
+if __name__ == "__main__":
+    main()
+    
---
+++ lesson8.py
@@ -0,0 +1,8 @@
+from tools import getStudent
+
+stu1 = getStudent(math=80,name="betty",english=70, chinese=70)
+print(stu1.name)
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.sum())
